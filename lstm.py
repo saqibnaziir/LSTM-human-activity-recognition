c18dd8f00bb8239dab340c60e2efdde71dd4050a
Saqib Nazir <37848312+saqibnaziir@users.noreply.github.com>
2020-06-27 12:02:41 +0500

    python

---
+++ lstm.py
@@ -0,0 +1,186 @@
+"""This code is wriiten to perform Human activity recognition using smartphones dataset and an LSTM RNN."""
+
+import tensorflow as tf
+
+import numpy as np
+def load_X(X_signals_paths):
+    X_signals = []
+
+    for signal_type_path in X_signals_paths:
+        file = open(signal_type_path, 'r')
+        # Read dataset from disk, dealing with text files' syntax
+        X_signals.append(
+            [np.array(serie, dtype=np.float32) for serie in [
+                row.replace('  ', ' ').strip().split(' ') for row in file
+            ]]
+        )
+        file.close()
+
+    return np.transpose(np.array(X_signals), (1, 2, 0))
+    def load_y(y_path):
+    file = open(y_path, 'r')
+    # Read dataset from disk, dealing with text file's syntax
+    y_ = np.array(
+        [elem for elem in [
+            row.replace('  ', ' ').strip().split(' ') for row in file
+        ]],
+        dtype=np.int32
+    )
+    file.close()
+       return y_ - 1
+
+
+class Config(object):
+def __init__(self, X_train, X_test):
+     
+        self.train_count = len(X_train)  
+        self.test_data_count = len(X_test) 
+        self.n_steps = len(X_train[0])  
+
+        self.learning_rate = 0.0025
+        self.lambda_loss_amount = 0.0015
+        self.training_epochs = 300
+        self.batch_size = 1500
+
+      
+        self.n_inputs = len(X_train[0][0])  # Features count is of 9: 3 * 3D sensors features over time
+        self.n_hidden = 32  # nb of neurons inside the neural network
+        self.n_classes = 6  # Final output classes
+        self.W = {
+            'hidden': tf.Variable(tf.random_normal([self.n_inputs, self.n_hidden])),
+            'output': tf.Variable(tf.random_normal([self.n_hidden, self.n_classes]))
+        }
+        self.biases = {
+            'hidden': tf.Variable(tf.random_normal([self.n_hidden], mean=1.0)),
+            'output': tf.Variable(tf.random_normal([self.n_classes]))
+        }
+
+
+def LSTM_Network(_X, config):
+_X = tf.transpose(_X, [1, 0, 2])  # permute n_steps and batch_size
+    _X = tf.reshape(_X, [-1, config.n_inputs])
+    _X = tf.nn.relu(tf.matmul(_X, config.W['hidden']) + config.biases['hidden'])
+    _X = tf.split(_X, config.n_steps, 0)
+
+    lstm_cell_1 = tf.contrib.rnn.BasicLSTMCell(config.n_hidden, forget_bias=1.0, state_is_tuple=True)
+    lstm_cell_2 = tf.contrib.rnn.BasicLSTMCell(config.n_hidden, forget_bias=1.0, state_is_tuple=True)
+    lstm_cells = tf.contrib.rnn.MultiRNNCell([lstm_cell_1, lstm_cell_2], state_is_tuple=True)
+    outputs, states = tf.contrib.rnn.static_rnn(lstm_cells, _X, dtype=tf.float32)
+
+    lstm_last_output = outputs[-1]
+
+    return tf.matmul(lstm_last_output, config.W['output']) + config.biases['output']
+
+
+def one_hot(y_):
+    """
+    Function to encode output labels from number indexes.
+
+    E.g.: [[5], [0], [3]] --> [[0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0]]
+    """
+    y_ = y_.reshape(len(y_))
+    n_values = int(np.max(y_)) + 1
+    return np.eye(n_values)[np.array(y_, dtype=np.int32)]
+
+
+if __name__ == "__main__":
+
+
+    INPUT_SIGNAL_TYPES = [
+        "body_acc_x_",
+        "body_acc_y_",
+        "body_acc_z_",
+        "body_gyro_x_",
+        "body_gyro_y_",
+        "body_gyro_z_",
+        "total_acc_x_",
+        "total_acc_y_",
+        "total_acc_z_"
+    ]
+
+
+    LABELS = [
+        "WALKING",
+        "WALKING_UPSTAIRS",
+        "WALKING_DOWNSTAIRS",
+        "SITTING",
+        "STANDING",
+        "LAYING"
+    ]
+
+    DATA_PATH = "data/"
+    DATASET_PATH = DATA_PATH + "UCI HAR Dataset/"
+    print("\n" + "Dataset is now located at: " + DATASET_PATH)
+    TRAIN = "train/"
+    TEST = "test/"
+
+    X_train_signals_paths = [
+        DATASET_PATH + TRAIN + "Inertial Signals/" + signal + "train.txt" for signal in INPUT_SIGNAL_TYPES
+    ]
+    X_test_signals_paths = [
+        DATASET_PATH + TEST + "Inertial Signals/" + signal + "test.txt" for signal in INPUT_SIGNAL_TYPES
+    ]
+    X_train = load_X(X_train_signals_paths)
+    X_test = load_X(X_test_signals_paths)
+
+    y_train_path = DATASET_PATH + TRAIN + "y_train.txt"
+    y_test_path = DATASET_PATH + TEST + "y_test.txt"
+    y_train = one_hot(load_y(y_train_path))
+    y_test = one_hot(load_y(y_test_path))
+
+
+    config = Config(X_train, X_test)
+    print("Some useful info to get an insight on dataset's shape and normalisation:")
+    print("features shape, labels shape, each features mean, each features standard deviation")
+    print(X_test.shape, y_test.shape,
+          np.mean(X_test), np.std(X_test))
+    print("the dataset is therefore properly normalised, as expected.")
+
+    X = tf.placeholder(tf.float32, [None, config.n_steps, config.n_inputs])
+    Y = tf.placeholder(tf.float32, [None, config.n_classes])
+
+    pred_Y = LSTM_Network(X, config)
+
+   
+    l2 = config.lambda_loss_amount * \
+        sum(tf.nn.l2_loss(tf_var) for tf_var in tf.trainable_variables())
+  
+    cost = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=pred_Y)) + l2
+    optimizer = tf.train.AdamOptimizer(
+        learning_rate=config.learning_rate).minimize(cost)
+
+    correct_pred = tf.equal(tf.argmax(pred_Y, 1), tf.argmax(Y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, dtype=tf.float32))
+
+   
+   
+    sess = tf.InteractiveSession(config=tf.ConfigProto(log_device_placement=False))
+    init = tf.global_variables_initializer()
+    sess.run(init)
+
+    best_accuracy = 0.0
+   
+    for i in range(config.training_epochs):
+        for start, end in zip(range(0, config.train_count, config.batch_size),
+                              range(config.batch_size, config.train_count + 1, config.batch_size)):
+            sess.run(optimizer, feed_dict={X: X_train[start:end],
+                                           Y: y_train[start:end]})
+
+       
+        pred_out, accuracy_out, loss_out = sess.run(
+            [pred_Y, accuracy, cost],
+            feed_dict={
+                X: X_test,
+                Y: y_test
+            }
+        )
+        print("traing iter: {},".format(i) +
+              " test accuracy : {},".format(accuracy_out) +
+              " loss : {}".format(loss_out))
+        best_accuracy = max(best_accuracy, accuracy_out)
+
+    print("")
+    print("final test accuracy: {}".format(accuracy_out))
+    print("best epoch's test accuracy: {}".format(best_accuracy))
+    print("")
